18ebbbc98fc54a1cf0a4b4461e2d38fe8b93fec8
ashokramcse <ashokramcse@gmail.com>
2018-01-18 11:00:58 +0530

    Initial Commit

---
+++ Basic_Demo.R
@@ -0,0 +1,25 @@
+x <- 1
+print (x)
+
+name<-"Ashok Ram"
+print (name)
+
+sequence<-20:50
+sequence
+
+array<-c(1,2,3,4)
+array
+
+typeCheck<-0:6
+class(typeCheck)
+
+# To change the data type use as.numeric, as.logical, as.charecter
+as.numeric(typeCheck)
+
+# Matrix
+m<-matrix(nrow=2,ncol=3)
+m<-matrix(1:6, nrow=2,ncol=3)
+
+dim(m)<-c(1,1)
+
+m<-1:10
